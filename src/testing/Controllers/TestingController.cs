c9142e53db8868d91ab842e50d26151ae43181ee
Flaminel <flm.marius@yahoo.com>
2022-09-12 00:05:14 +0300

    test 1

---
+++ src/testing/Controllers/TestingController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace testing.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class TestingController : ControllerBase
+{
+    private readonly ILogger<TestingController> _logger;
+
+    public TestingController(ILogger<TestingController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Get()
+    {
+        await Task.CompletedTask;
+
+        return Ok("Testing 1.");
+    }
+}
---
+++ src/testing/Program.cs
@@ -0,0 +1,56 @@
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Hosting;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new() { Title = "testing", Version = "v1" });
+});
+builder.Services.AddHealthChecks()
+    .AddCheck("self", () => HealthCheckResult.Healthy(), new[] { "self" })
+    .AddCheck("services", () => HealthCheckResult.Healthy(), new[] { "services" });
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (builder.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "testing v1"));
+}
+
+// Health check for liveliness
+app.UseHealthChecks("/api/health/live", new HealthCheckOptions
+{
+    Predicate = r => r.Name.Contains("self"),
+    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+});
+
+// Health check for readiness
+app.UseHealthChecks("/api/health/ready", new HealthCheckOptions
+{
+    Predicate = r => r.Tags.Contains("services"),
+    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+});
+
+app.UseAuthorization();
+
+app.UseCors(builder =>
+{
+    builder.AllowAnyOrigin()
+        .AllowAnyMethod()
+        .AllowAnyHeader();
+});
+
+app.MapControllers();
+
+app.Run();
