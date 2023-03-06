ab53e425e15fdeaf5b64a2c6e0c50caf63bfc4e9
Flaminel <flm.marius@yahoo.com>
2023-03-06 10:07:21 +0200

    fixed build workflow; added deploy workflow

--- code/testing/Controllers/TestingController.cs
+++ code/testing/Controllers/TestingController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace testing.Controllers;
@@ -20,6 +23,22 @@ public class TestingController : ControllerBase
     {
         await Task.CompletedTask;
 
-        return Ok("Testing 1.");
+        var jsonSettings = new JsonSerializerSettings
+        {
+            ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
+            Formatting = Formatting.Indented
+        };
+
+        StringBuilder stringBuilder = new();
+        stringBuilder.AppendLine("############ Start");
+        stringBuilder.AppendLine($"Headers: {JsonConvert.SerializeObject(HttpContext.Request.Headers, jsonSettings)}");
+        stringBuilder.AppendLine($"Host: {HttpContext.Request.Host}");
+        var conn = HttpContext.Connection;
+        stringBuilder.AppendLine($"Connection: {conn.LocalIpAddress}:{conn.LocalPort}->{conn.RemoteIpAddress}:{conn.RemotePort}");
+        stringBuilder.AppendLine("############ End");
+
+        Console.WriteLine(stringBuilder.ToString());
+
+        return Ok();
     }
 }
